c6fa45f1b64eb9019d2c648f834d19f805fa6461
DavyOprendek <davy.oprendek.28@gmail.com>
2022-03-31 14:20:38 +0200

    Replace name
    Remplace le nom afin d'éviter tout problème

---
+++ BotMusique.js
@@ -0,0 +1,39 @@
+var Discord = require("discord.js");
+var Client = new Discord.Client({ intents: [] });
+const ytdl = require("ytdl-core");
+// const amount = client.sweepMessages(7200);
+
+Client.on("ready", () => {
+    console.log("Ready")
+
+});
+
+Client.on("message", message => {
+    if (message.content.startsWith(prefix+ "play")) {
+        if (message.member.voice.channel) {
+            message.member.voice.channel.join().then(Connection => {
+                if (!arg[1]) {
+                    message.reply("Lien de la video incorrecte ou indisponible")
+                }
+                else {
+                    let args = message.content.split("");
+                    let dispatcher = connection.play(ytdl(args[1], { quality: "highestaudio", highWaterMark: 1 << 25 }));
+                    dispatcher.on("finish", () => {
+                        dispatcher.destroy();
+                    });
+                    dispatcher.on("error"), err => {
+                        console.log("Probleme avec le dispatch : " + erreur);
+                    };
+                }
+            }).catch(erreur => {
+                message.reply("Probleme de connexion : " + erreur)
+            });
+        }
+        else {
+            message.reply("Connecte toi a un channel vocal");
+        }
+    }
+
+if(message.content.startsWith(prefix+"clear"))
+    console.log(`Successfully removed ${amount} messages from the cache.`);
+});
